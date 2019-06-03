d0b8b7be0d93d3fb08a17a8fc6e53bd61247d499
ruwanliyanage <ruwanliyanagegalle1994@gmail.com>
2019-06-03 16:52:50 +0530

    add html content

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
-  selector: 'app-sign-up',
+  selector: 'ngx-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  constructor(public authService: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
